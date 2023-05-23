d509824ea260dcc3de6c5e9ce0f7b2e1cc69625e
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-23 13:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function decimalToBinary(decimal) {
-
-  let binary = "";
-  if (decimal === 0) {
-    return "0";
-  }
+  let binary = '';
   while (decimal > 0) {
-    binary = (decimal % 2) + binary;
+    const remainder = decimal % 2;
+    binary = remainder + binary;
     decimal = Math.floor(decimal / 2);
   }
   return binary;
 }
 
-// Test the function with example inputs
-const decimal1 = 7;
-const binary1 = decimalToBinary(decimal1);
-console.log(`Decimal: ${decimal1}\nBinary: ${binary1}\n`);
+/*Do not change anything below*/
 
-const decimal2 = 10;
-const binary2 = decimalToBinary(decimal2);
-console.log(`Decimal: ${decimal2}\nBinary: ${binary2}\n`);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
 
-const decimal3 = 33;
-const binary3 = decimalToBinary(decimal3);
-console.log(`Decimal: ${decimal3}\nBinary: ${binary3}`);
+rl.on('line', function (line) {
+  const decimal = parseInt(line.trim());
+  const binary = decimalToBinary(decimal);
+  console.log(binary);
+  rl.close();
+});
