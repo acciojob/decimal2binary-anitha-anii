9cea5ffcefe731de754f19180b63766558f3f4ce
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-02 18:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function decimalToBinary(dec) {
-  return dec.toString(2);
+
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-console.log(decimalToBinary(7)); 
+ 
+console.log(decimalToBinary(7));
 console.log(decimalToBinary(10)); 
-console.log(decimalToBinary(33));
+console.log(decimalToBinary(33)); 
